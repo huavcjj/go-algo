fe45f4e10e36124c0edabdc612e956877effbb72
huavcjj <huavcjj0217@icloud.com>
2024-12-11 17:12:38 +0900

    feat

---
+++ fibonacci/fibonacci.go
@@ -0,0 +1,68 @@
+package algorithm
+
+func Fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return Fib(n-1) + Fib(n-2)
+}
+
+func FibTopDown(n int) int {
+	if n <= 1 {
+		return n
+	}
+	cache := make(map[int]int)
+	cache[0] = 0
+	cache[1] = 1
+
+	return f(n, cache)
+}
+
+func f(n int, cache map[int]int) int {
+	if val, exists := cache[n]; exists {
+		return val
+	}
+	cache[n] = f(n-1, cache) + f(n-2, cache)
+	return cache[n]
+}
+
+func FibButtomUp(n int) int {
+	if n <= 1 {
+		return n
+	}
+	arr := make([]int, n+1)
+	arr[0] = 0
+	arr[1] = 1
+	for i := 2; i <= n; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr[n]
+}
+
+func FibButtomUp01(n int) int {
+	if n <= 1 {
+		return n
+	}
+	prev2 := 0
+	prev1 := 1
+	for i := 2; i <= n; i++ {
+		curr := prev1 + prev2
+		prev2 = prev1
+		prev1 = curr
+	}
+	return prev1
+}
+
+func Steps(n int) int {
+	if n <= 2 {
+		return n
+	}
+	prev2 := 1
+	prev1 := 2
+	for i := 3; i <= n; i++ {
+		curr := prev1 + prev2
+		prev2 = prev1
+		prev1 = curr
+	}
+	return prev1
+}
---
+++ fibonacci/fibonacci_test.go
@@ -0,0 +1,43 @@
+package algorithm
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFib(t *testing.T) {
+	n := 20
+	a, b, c, d := Fib(n), FibTopDown(n), FibButtomUp(n), FibButtomUp01(n)
+	if a != b || b != c || c != d {
+		fmt.Println(a, b, c, d)
+		t.Fail()
+	}
+}
+
+func BenchmarkFib(b *testing.B) {
+	n := 20
+	for i := 0; i < b.N; i++ {
+		Fib(n)
+	}
+}
+
+func BenchmarkFibTopDown(b *testing.B) {
+	n := 20
+	for i := 0; i < b.N; i++ {
+		FibTopDown(n)
+	}
+}
+
+func BenchmarkFibButtomUp(b *testing.B) {
+	n := 20
+	for i := 0; i < b.N; i++ {
+		FibButtomUp(n)
+	}
+}
+
+func BenchmarkFibButtomUp01(b *testing.B) {
+	n := 20
+	for i := 0; i < b.N; i++ {
+		FibButtomUp01(n)
+	}
+}
