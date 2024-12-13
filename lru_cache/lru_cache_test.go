4112abf2a1a954e923701c75ecd8b04d27036455
huavcjj <huavcjj0217@icloud.com>
2024-12-14 00:27:46 +0900

    feat

--- lru_cache/lru_cache_test.go
+++ lru_cache/lru_cache_test.go
@@ -1,13 +1,13 @@
 package algorithm
 
 import (
-	"fmt"
 	"strconv"
 	"testing"
 )
 
 func TestLRUCache(t *testing.T) {
 	lru := NewLRUCache(10)
+
 	for i := 0; i < 10; i++ {
 		lru.Add(i, strconv.Itoa(i)) //9 8 7 6 5 4 3 2 1 0
 	}
@@ -21,8 +21,15 @@ func TestLRUCache(t *testing.T) {
 	}
 
 	for i := 0; i < 10; i++ {
-		if v, ok := lru.Get(i); !ok || v != strconv.Itoa(i) {
-			fmt.Printf("key %d not found\n", i)
+		v, ok := lru.Get(i)
+		if i%2 == 0 { // 0, 2, 4, 6, 8 はキャッシュに残るべき
+			if !ok || v != strconv.Itoa(i) {
+				t.Errorf("key %d not found or incorrect value: got %v, want %v", i, v, strconv.Itoa(i))
+			}
+		} else { // 1, 3, 5, 7, 9 はキャッシュから削除されるべき
+			if ok {
+				t.Errorf("key %d should not be found, got %v", i, v)
+			}
 		}
 	}
 }
